eaadc7f8bc3ba5b04bcc03d7e35cf01c71d9cbec
Romanets Pavel <pavelroma24@gmail.com>
2022-08-01 07:40:49 +0300

    add: l_3_4

--- l_3_1/index.js
+++ l_3_1/index.js
@@ -11,7 +11,11 @@ const app = express();
 
 const hgs = exphbs.create({
     defaultLayout: 'main',
-    extname: 'hbs'
+    extname: 'hbs',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    }
 });
 
 app.engine('hbs', hgs.engine);
