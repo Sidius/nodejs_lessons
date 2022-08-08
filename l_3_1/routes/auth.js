7a0b905cde08a420f297eb035434b41389e08050
Romanets Pavel <pavelroma24@gmail.com>
2022-08-08 08:17:26 +0300

    add: l_4_2

--- l_3_1/routes/auth.js
+++ l_3_1/routes/auth.js
@@ -8,4 +8,22 @@ router.get('/login', async (req, res) => {
     });
 });
 
+router.get('/logout', async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login#login');
+    });
+});
+
+router.post('/login', async (req, res) => {
+    req.session.isAuthenticated = true;
+    res.redirect('/');
+});
+
+router.get('/register', async (req, res) => {
+    res.render('auth/register', {
+        title: 'Authorization',
+        isLogin: true
+    });
+});
+
 module.exports = router;
