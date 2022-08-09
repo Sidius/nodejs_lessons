ae7c12cc3a69bd846f65855016d002881dc3eb70
Romanets Pavel <pavelroma24@gmail.com>
2022-08-09 08:37:19 +0300

    add: l_4_8

--- l_4_3/routes/auth.js
+++ l_4_3/routes/auth.js
@@ -16,14 +16,33 @@ router.get('/logout', async (req, res) => {
 });
 
 router.post('/login', async (req, res) => {
-    req.session.user = await User.findById('62e8c76fc7c9692f1d38a368');
-    req.session.isAuthenticated = true;
-    req.session.save(err => {
-        if (err) {
-            throw err;
+    try {
+        const {email, password} = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+            const areSame = password === candidate.password;
+
+            if (areSame) {
+                req.session.user = candidate;
+                req.session.isAuthenticated = true;
+                req.session.save(err => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.redirect('/');
+                });
+            } else {
+                res.redirect('/auth/login#login');
+            }
+        } else {
+            res.redirect('/auth/login#login');
         }
-        res.redirect('/');
-    });
+    } catch (e) {
+        console.log(e);
+    }
+    
+
 });
 
 router.get('/register', async (req, res) => {
@@ -40,16 +59,15 @@ router.post('/register', async (req, res) => {
 
         if (candidate) {
             res.redirect('/auth/login#register');
+        } else {
+            const user = new User({
+                email, name, password, cart: {
+                    items: []
+                }
+            });
+            await user.save();
+            res.redirect('/auth/login');
         }
-
-        const user = new User({
-            email, name, password, cart: {
-                items: []
-            }
-        });
-
-        await user.save();
-        res.redirect('/auth/login');
     } catch (e) {
         console.log(e);
     }
