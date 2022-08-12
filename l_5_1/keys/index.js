3381147bd934f9ec2991373dfad0be92e073324d
Romanets Pavel <pavelroma24@gmail.com>
2022-08-12 07:15:32 +0300

    add: l_5_1 (Keys)

---
+++ l_5_1/keys/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB_URI: `mongodb+srv://romanets:k1ZSvdCGaKFqEcv5@cluster0.t2dugdk.mongodb.net/shop`,
+    SESSION_SECRET: 'some secret value'
+}
---
+++ l_5_1/routes/auth.js
@@ -0,0 +1,83 @@
+const {Router} = require('express');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
+const router = Router();
+
+router.get('/login', async (req, res) => {
+    res.render('auth/login', {
+        title: 'Authorization',
+        isLogin: true,
+        loginError: req.flash('loginError'),
+        registerError: req.flash('registerError'),
+    });
+});
+
+router.get('/logout', async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect('/auth/login#login');
+    });
+});
+
+router.post('/login', async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+            const areSame = await bcrypt.compare(password, candidate.password);
+
+            if (areSame) {
+                req.session.user = candidate;
+                req.session.isAuthenticated = true;
+                req.session.save(err => {
+                    if (err) {
+                        throw err;
+                    }
+                    res.redirect('/');
+                });
+            } else {
+                req.flash('loginError', 'Wrong password');
+                res.redirect('/auth/login#login');
+            }
+        } else {
+            req.flash('loginError', 'This user do not exist');
+            res.redirect('/auth/login#login');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+    
+
+});
+
+router.get('/register', async (req, res) => {
+    res.render('auth/register', {
+        title: 'Authorization',
+        isLogin: true
+    });
+});
+
+router.post('/register', async (req, res) => {
+    try {
+        const {email, password, repeat, name} = req.body;
+        const candidate = await User.findOne({email});
+
+        if (candidate) {
+            req.flash('registerError', 'User already exists with the same email');
+            res.redirect('/auth/login#register');
+        } else {
+            const hashPassword = await bcrypt.hash(password, 10);
+            const user = new User({
+                email, name, password: hashPassword, cart: {
+                    items: []
+                }
+            });
+            await user.save();
+            res.redirect('/auth/login');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+module.exports = router;
