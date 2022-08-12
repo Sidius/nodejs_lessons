c7373175569012549cd6507cda3f6227e2bd3314
Romanets Pavel <pavelroma24@gmail.com>
2022-08-12 07:05:21 +0300

    add: l_4_11 (Alert messages)

--- l_4_3/routes/auth.js
+++ l_4_3/routes/auth.js
@@ -6,7 +6,9 @@ const router = Router();
 router.get('/login', async (req, res) => {
     res.render('auth/login', {
         title: 'Authorization',
-        isLogin: true
+        isLogin: true,
+        loginError: req.flash('loginError'),
+        registerError: req.flash('registerError'),
     });
 });
 
@@ -34,9 +36,11 @@ router.post('/login', async (req, res) => {
                     res.redirect('/');
                 });
             } else {
+                req.flash('loginError', 'Wrong password');
                 res.redirect('/auth/login#login');
             }
         } else {
+            req.flash('loginError', 'This user do not exist');
             res.redirect('/auth/login#login');
         }
     } catch (e) {
@@ -59,6 +63,7 @@ router.post('/register', async (req, res) => {
         const candidate = await User.findOne({email});
 
         if (candidate) {
+            req.flash('registerError', 'User already exists with the same email');
             res.redirect('/auth/login#register');
         } else {
             const hashPassword = await bcrypt.hash(password, 10);
