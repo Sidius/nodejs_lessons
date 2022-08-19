4f92b36ea912b1688703ce114aab3066c3bf133c
Romanets Pavel <pavelroma24@gmail.com>
2022-08-19 06:11:30 +0300

    add: l_6_4

--- l_6_1/utils/validators.js
+++ l_6_1/utils/validators.js
@@ -2,25 +2,27 @@ const {body} = require('express-validator');
 const User = require('../models/user');
 
 exports.registerValidators = [
-    body('email').isEmail().withMessage('Type correct email').custom(async (value, {req}) => {
-        try {
-            const user = await User.findOne({ email: value });
-            if (user) {
-                return Promise.reject('This email is in base');
+    body('email').isEmail().withMessage('Type correct email')
+        .custom(async (value, {req}) => {
+            try {
+                const user = await User.findOne({ email: value });
+                if (user) {
+                    return Promise.reject('This email is in base');
+                }
+            } catch (e) {
+                console.log(e);
             }
-        } catch (e) {
-            console.log(e);
-        }
-    }),
+         })
+        .normalizeEmail(),
     body('password', 'Password must be more than 5 symbols').isLength({
         min: 6,
         max: 56
-    }).isAlphanumeric(),
+    }).isAlphanumeric().trim(),
     body('confirm').custom((value, {req}) => {
         if (value !== req.body.password) {
             throw new Error('Passwords have to be same');
         }
         return true;
-    }),
-    body('name').isLength({min: 3}).withMessage('Name must be more than 3 symbols'),
+    }).trim(),
+    body('name').isLength({min: 3}).withMessage('Name must be more than 3 symbols').trim(),
 ];
